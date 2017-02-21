aad7c32f493650d509c9d05f7d62f089f657c44e
Jacob Mittelstaedt <jacobmittelstaedt@jacobs-macbook-pro.local>
2017-02-21 16:06:58 -0500

    Initial Commit

---
+++ TwitterProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterProj
+//
+//  Created by Jacob Mittelstaedt on 2/21/17.
+//  Copyright © 2017 Jacob Mittelstaedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
